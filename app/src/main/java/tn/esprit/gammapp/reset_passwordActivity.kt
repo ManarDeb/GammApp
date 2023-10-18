8925734c3c1e7aec2fa718241da4ed4cdcbdc8e0
ManarDeb <123480744+ManarDeb@users.noreply.github.com>
2023-10-18 19:06:27 +0100

    Initial commit

---
+++ app/src/main/java/tn/esprit/gammapp/reset_passwordActivity.kt
@@ -0,0 +1,11 @@
+package tn.esprit.gammapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reset_passwordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
